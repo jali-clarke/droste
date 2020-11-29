11fd1b6c589b5bf8b7c0a0b809e3684acac774d6
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-11-29 03:22:21 +0000

    create static dir if not exists

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Network.Wai.Handler.Warp
 import Network.Wai.Logger
 import Servant
+import System.Directory
 import System.Environment
 
 import Api
@@ -15,6 +16,7 @@ main :: IO ()
 main = do
     staticDir <- getEnv "STATIC_DIR"
     serverPort <- getEnv "SERVER_PORT" >>= readIO
+    createDirectoryIfMissing False staticDir
     withStdoutLogger $ \logger ->
         let settings = setPort serverPort . setLogger logger $ defaultSettings
         in runSettings settings (serve api (server staticDir))
